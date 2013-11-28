c88d88cf79c3033923581a8f887d74300e858625
MySmallfish <mysmallfish@gmail.com>
2013-11-29 00:26:07 +0200

    fixes, animations, splash

--- app/js/controllers/splash.js
+++ app/js/controllers/splash.js
@@ -1,9 +1,30 @@
 ﻿(function (S, C, Y) {
 
-    Y.SplashController = function ($scope, $location, accountManager, metadataService, utils, alertService) {
-        
+    Y.SplashController = function ($scope, $location, accountManager, metadataService, utils, alertService, $timeout) {
+
+        $scope.step = 0;
+
+        function step() {
+            $timeout(function() {
+                if ($scope.step < 6) {
+                    $scope.step++;
+                    step();
+                } else {
+                    $timeout(navigate, 750);
+                }
+            }, 250);
+        }
+
+        step();
+
+        function navigate() {
+            if ($scope.step == 6) {
+                $location.path("/Login");
+            }
+        }
+
         metadataService.fetchMetadata().then(function() {
-            $location.path("/Login");
+            navigate();
         }, function (error) {
             if (C.isError(error, Y.Errors.VersionRequired, C.Severity.Warning)) {
                 alertService.show(error.Dialog).then(function (result) {
