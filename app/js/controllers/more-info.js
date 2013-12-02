a743e392d2b97dba18f451ca8144757afbd23140
MySmallfish <mysmallfish@gmail.com>
2013-12-02 17:22:56 +0200

    legal tems

--- app/js/controllers/more-info.js
+++ app/js/controllers/more-info.js
@@ -1,7 +1,11 @@
 ﻿(function (S, C, Y) {
     
-    Y.MoreInfoController = function ($scope, $location) {
+    Y.MoreInfoController = function ($scope, metadataService) {
         $scope.$root.header = "More Info";
+        metadataService.getMetadata().then(function(metadata) {
+            $scope.email = metadata.ServiceEmailAdd;
+            $scope.fax = metadata.ServicePhone2;
+        });
     };
     
 })(Simple, Cal, Cal.Yazil);
