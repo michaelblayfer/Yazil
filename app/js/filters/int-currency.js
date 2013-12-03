831cff25013d93c3203c8748694e56544d24b9d4
MySmallfish <mysmallfish@gmail.com>
2013-12-03 13:17:30 +0200

    Currency display

--- app/js/filters/int-currency.js
+++ app/js/filters/int-currency.js
@@ -3,9 +3,11 @@
     ['$filter', '$locale',
           function (filter, locale) {
               var currencyFilter = filter('currency');
+              
               var formats = locale.NUMBER_FORMATS;
               return function (amount, currencySymbol) {
                   var value = currencyFilter(amount, currencySymbol);
+                  return value;
                   var sep = value.indexOf(formats.DECIMAL_SEP);
                   if (amount >= 0) {
                       return value.substring(0, sep);
