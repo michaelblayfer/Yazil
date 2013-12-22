d5c50041f99bb7e4306deb6f73686a8c9f4a01c7
MySmallfish <mysmallfish@gmail.com>
2013-12-22 18:19:08 +0200

    test

--- app/js/controllers/splash.js
+++ app/js/controllers/splash.js
@@ -1,6 +1,6 @@
 ﻿(function (S, C, Y) {
 
-    Y.SplashController = function ($scope, $location, accountManager, metadataService, utils, alertService, $timeout) {
+    Y.SplashController = function ($scope, $rootScope, $location, accountManager, metadataService, utils, alertService, $timeout) {
 
         $scope.step = 0;
 
@@ -9,28 +9,28 @@
                 if ($scope.step < 6) {
                     $scope.step++;
                     step();
-                } else {
-                    $timeout(navigate, 750);
-                }
+                } 
             }, 250);
         }
 
         step();
 
-        function navigate() {
-            if ($scope.step == 6) {
-                $location.path("/Login");
-            }
-        }
 
-        metadataService.fetchMetadata().then(function(metadata) {
+        function navigate() {
+            $location.path("/Login");
+        }
+        return metadataService.fetchMetadata().then(function (metadata) {
             navigate();
+            return metadata;
         }, function (error) {
             if (C.isError(error, Y.Errors.VersionRequired, C.Severity.Warning)) {
-                alertService.show(error.Dialog).then(function (result) {
-                    if (result.status == "Confirm") {
-                        window.open(utils.os.isIOS() ? error.data.UpdateURLIOS : error.data.UpdateURLAndroid);
-                    }
+                var dialog = error.Dialog;
+                dialog.overrideDefault = true;
+                dialog.dontDismiss = true;
+                alertService.show(dialog).then(function (result) {
+                    var versionUpdateUrl = error.data.UpdateURL;
+                    utils.browser.open(versionUpdateUrl);
+
                 });
             } else {
                 alertService.show(error.Dialog || {});
