bc1404279a145c2ea83022d1c72c1a700ae93e64
michaelblayfer <michael.blayfer@icc.co.il>
2014-03-17 13:21:48 +0200

    Update routes.js

--- app/js/routes.js
+++ app/js/routes.js
@@ -13,12 +13,6 @@
     yazilModule.config(function ($routeProvider) {
         $routeProvider
             .when("/", { templateUrl: "views/home.html", controller: "HomeCtrl", resolve: { pageInfo: function () { return { header: "MyAccountsTitle" }; } } })
-            .when("/Login", { templateUrl: "views/login.html", controller: "LoginCtrl", resolve: { pageInfo: function () { return { header: "Welcome", hideFooter: true }; } } })
-            .when("/Account", { templateUrl: "views/account.html", controller: "AccountCtrl", resolve: { pageInfo: function () { return { header: "MyAccountTitle" }; } } })
-            .when("/LegalTerms", { templateUrl: "views/legal-terms.html", controller: "MoreInfoCtrl", resolve: { pageInfo: function () { return { header: "LegalTerms", displayBack: true }; } } })
-            .when("/Security", { templateUrl: "views/security.html", controller: "MoreInfoCtrl", resolve: { pageInfo: function () { return { header: "InfoSecurity", displayBack: true }; } } })
-            .when("/MoreInfo", { templateUrl: "views/more-info.html", controller: "MoreInfoCtrl", resolve: { pageInfo: function () { return { header: "MoreInfo" }; } } })
-            .when("/CustomerService", { templateUrl: "views/customer-service.html", controller: "CustomerServiceCtrl", resolve: { pageInfo: function () { return { header: "CustomerService" }; } } })
             .otherwise({ redirectTo: "/" });
     });
 
