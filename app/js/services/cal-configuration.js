7efa9e685e6d2264aacca05f1ce1bbbe6adf815d
michaelblayfer <michael.blayfer@icc.co.il>
2014-03-17 12:19:40 +0200

    Update cal-configuration.js

--- app/js/services/cal-configuration.js
+++ app/js/services/cal-configuration.js
@@ -3,12 +3,16 @@
         function getValue(name){
             return $("head meta[name='" + name + "']").attr("content");
         }
-
+        
+        var _os = "Other";
+        if (utils.os.isIOS()) _os = "IOS";
+        if (utils.os.isAndroid() _os = "Android";
+        
         var config = {
             senderID: "1011010555758",
             baseUrl: getValue("base-url"),
             version: getValue("version"),
-            os: (utils.os.isIOS() ? "IOS" : (utils.os.isAndroid() ? "Android" : "Other"))
+            os: _os
         };
         
         return config;
