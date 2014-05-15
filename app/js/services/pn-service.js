d5ddd51e80441bf11fa71c5cdc55bc6be45de03c
michaelblayfer <michael.blayfer@icc.co.il>
2014-05-15 15:32:36 +0300

    Update pn-service.js

--- app/js/services/pn-service.js
+++ app/js/services/pn-service.js
@@ -93,7 +93,7 @@
                 case 'message':               // this is the actual push notification. its format depends on the data model from the push server              
                     $rootScope.$emit("PN_message", e);
                     console.log('message = ' + e.message + ' msgcnt = ' + e.msgcnt);
-                    console.log('whole event body = ' + JSON.stringify(e));
+                    console.log('custom payload = ' + JSON.stringify(e.payload.custom));
                     break;
 
                 case 'error':
